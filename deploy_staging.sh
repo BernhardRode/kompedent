ba56d668539aa658df956b7fea1870f96a4d527f
Bernhard Rode <bernhard.rode@1und1.de>
2015-04-14 14:26:34 +0000

    Moved images to public folder, added a robots.txt, bootstrap is now being built without glyphicons.

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-rm -Rf www
-./node_modules/.bin/bower install
-./node_modules/.bin/harp compile
-./node_modules/.bin/surge www jaded-balloon.surge.sh
+#rm -Rf www
+harp compile
+surge www jaded-balloon.surge.sh
