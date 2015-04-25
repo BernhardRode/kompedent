70591ed18eddb9fb6b06c89719335ebf7acab45c
Bernhard Rode <mail@bernhardrode.de>
2015-04-25 08:15:59 +0200

    More fixes!

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
-$(document).ready(function(){
-  // cache the window object
+(function(factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery', 'hammerjs'], factory);
+  } else if (typeof exports === 'object') {
+    factory(require('jquery'), require('hammerjs'));
+  } else {
+    factory(jQuery, Hammer);
+  }
+}(function($, Hammer) {
+  function hammerify(el, options) {
+    var $el = $(el);
+    if(!$el.data("hammer")) {
+      $el.data("hammer", new Hammer($el[0], options));
+    }
+  }
+
+  $.fn.hammer = function(options) {
+    return this.each(function() {
+      hammerify(this, options);
+    });
+  };
+
+  // extend the emit method to also trigger jQuery events
+  Hammer.Manager.prototype.emit = (function(originalEmit) {
+    return function(type, data) {
+      originalEmit.call(this, type, data);
+      $(this.element).trigger({
+        type: type,
+        gesture: data
+      });
+    };
+  })(Hammer.Manager.prototype.emit);
+}));
+
+
+$(document).ready(function () {
   $window = $(window);
-  //$body= $('body');
 
   //smooth scroll
-  $('a[href*=#]').click(function() {
-    if ( !$(this).attr( 'data-slide') ) {
+  $('a[href*=#]').click(function () {
+    if (!$(this).attr('data-slide')) {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var $target = $(this.hash);
         $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
@@ -18,33 +51,41 @@ $(document).ready(function(){
     };
   });
 
-  if( $('#sidebar').length ) {
-    function onScroll(event) { 
+  if ($('.carousel').length) {
+    $('.carousel').hammer();
+
+    $('.carousel').on('swipeleft', function() {
+      $(this).carousel('prev');
+    });
+
+    $('.carousel').on('swiperight', function() {
+      $(this).carousel('next');
+    });
+  }
+
+  if ($('#sidebar').length) {
+    function onScroll(event) {
       var scrollPosition = $(document).scrollTop();
       $('#sidebar a').each(function () {
         var currentLink = $(this);
-	var refElement = $(currentLink.attr("href"));
-	console.log("Position",scrollPosition);
-        console.log("From",refElement.position().top);
- 	console.log("To", refElement.position().top + refElement.height());
-	if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-	  $('#sidebar ul li a').removeClass("active");
-          console.log('Found');
-	  currentLink.addClass("active");
-	} else {
-          console.log('Not Found');
-	  currentLink.removeClass("active");
-	};
-	console.log('###');
+        var refElement = $(currentLink.attr("href"));
+        if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+          $('#sidebar ul li a').removeClass("active");
+          currentLink.addClass("active");
+        } else {
+          currentLink.removeClass("active");
+        };
+
       });
     }
+
     $('#sidebar a').first().addClass('active');
     $(document).on("scroll", onScroll);
   }
 
-  if( $('.parallax').length ) {
-    setTimeout( function() {
+  if ($('.parallax').length) {
+    setTimeout(function () {
       $(window).trigger('resize').trigger('scroll');
-    },600);
+    }, 600);
   }
 });
